202ec1fa032932f3111fd563d6b6c1c5cd084b80
cloesd22 <cloesd22@gmail.com>
2017-06-29 04:36:23 +1000

    initial commit of class

---
+++ stringworkshop.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 04:23:59 2017
+
+@author: manoj
+"""
+
+##String Processor, capable of doing advanced mass string functions such as
+#word count, word frequency calculation etc.
+
+
+class stringworkshop():
+    
+    def wordcount(self,word):
+        word = word.split()
+        #print(len(word))
+        return word
+    
+    
+        
+        
